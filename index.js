2d0f99d811999d56291f2074a3cb1fad09d2aea1
Esteban Lapissonde <esteban.lapissonde@gmail.com>
2021-03-29 18:59:02 -0300

    changes url server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import patientsRoutes from "./routes/patients.js";
 
 const app = express();
@@ -14,12 +14,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/patients", patientsRoutes);
+app.use(express.static("build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 // const CONNECTION_URL = "mongodb://localhost/clinic";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
